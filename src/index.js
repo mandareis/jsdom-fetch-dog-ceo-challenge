84c26b1877a9702f4868612fe45299f9efaec225
mandareis <amanda@depaula.xyz>
2020-11-19 07:31:17 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+async function dogCeo() {
+  const response = await fetch(imgUrl);
+  const responseJson = await response.json();
+  console.log(responseJson);
+  let imgs = responseJson.message.map((imgUrl) => {
+    let img = document.createElement("img");
+    img.src = imgUrl;
+    return img;
+  });
+  const imageContainer = document.getElementById("dog-image-container");
+  imgs.forEach((img_1) => {
+    imageContainer.appendChild(img_1);
+  });
+}
+dogCeo();
+
+async function buildBreedList() {
+  const res = await fetch(breedUrl);
+  const resJson = await res.json();
+  console.log(resJson);
+  const breedList = document.getElementById("dog-breeds");
+  for (const property in resJson.message) {
+    console.log(`${property}: ${resJson.message[property]}`);
+    const li = document.createElement("li");
+    li.addEventListener("click", addColor);
+    li.innerText = property;
+    breedList.appendChild(li);
+  }
+}
+buildBreedList();
+function addColor(event) {
+  event.target.style = "color: red";
+}
